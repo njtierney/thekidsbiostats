dc63d7d74f22b5f5507445d29dca9a8b425edf8b
wesdb <wesley.billingham@telethonkids.org.au>
2024-12-10 14:50:41 +0800

    added desc_plot, dependent on by

--- R/model_helpers.R
+++ R/model_helpers.R
@@ -16,18 +16,56 @@
 #'
 #' @exportS3Method thekidsbiostats::thekids_model_output
 thekids_model_output.lm <- function(mod, by, dat_mod = NULL, ...) {
-  print(paste("Model class:", class(mod)))
 
   if(is.null(dat_mod)) dat_mod <- mod$model
 
-  mod_desc <- dat_mod %>%
-    tbl_summary(by = by,
-                type = list(where(is.numeric) ~ "continuous"),
-                statistic = list(all_continuous() ~ "{mean} ({sd}) [{N_nonmiss}]")) %>%
-    modify_header(label = by) %>%
-    add_p() %>%
-    bold_labels() %>%
-    suppressMessages() %>% suppressWarnings()
+  y <- all.vars(formula(mod))[1]
+
+  if(is.character(dat_mod[[by]])) {
+    mod_desc <- dat_mod %>%
+      tbl_summary(by = by,
+                  type = list(where(is.numeric) ~ "continuous"),
+                  statistic = list(all_continuous() ~ "{mean} ({sd}) [{N_nonmiss}]")) %>%
+      modify_header(label = by) %>%
+      add_p() %>%
+      bold_labels() %>%
+      suppressMessages() %>% suppressWarnings()
+
+    mod_desc_plot <- dat_mod %>%
+      mutate(x = factor(.[[by]])) %>%
+      ggplot(aes(x = x, y = .data[[y]],
+                 colour = x, group = x, fill = x)) +
+      geom_violin(alpha = 0.1, width = 0.5) +
+      geom_jitter(height = 0, width = 0.05, alpha = 0.3) +
+      stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.05, colour = "red") +
+      stat_summary(fun = mean, colour = "red", geom="point", size = 4) +
+      stat_summary(fun = mean, colour = "black", geom="point", size = 3, shape = 4) +
+      thekids_theme() +
+      theme(plot.caption = element_text(size = 12, face = "italic"),
+            legend.position = "none") +
+      labs(title = "Jittered dot and violin distribution plot",
+           subtitle = paste0(y, " plotted by ", by),
+           caption = "Black 'X' on red circle indicates mean (±SE)",
+           x = by)
+  } else {
+    mod_desc <- dat_mod %>%
+      tbl_summary(type = list(where(is.numeric) ~ "continuous"),
+                  statistic = list(all_continuous() ~ "{mean} ({sd}) [{N_nonmiss}]")) %>%
+      bold_labels() %>%
+      suppressMessages() %>% suppressWarnings()
+
+    mod_desc_plot <- dat_mod %>%
+      mutate(x = .[[by]]) %>%
+      ggplot(aes(x = x, y = .data[[y]])) +
+      geom_jitter(width = 0.05, height = 0.05, alpha = 0.6, col = thekids_colours[[3]]) +
+      geom_smooth(fill = thekids_colours[[1]], col = thekids_colours[[1]]) +
+      thekids_theme() +
+      theme(plot.caption = element_text(size = 12, face = "italic"),
+            legend.position = "none") +
+      labs(title = "Dot plot with line of best bit (LOESS)",
+           subtitle = paste0(y, " plotted by ", by),
+           x = by)
+  }
 
   mod_diag <- autoplot(mod)
 
@@ -40,8 +78,10 @@ thekids_model_output.lm <- function(mod, by, dat_mod = NULL, ...) {
       rows = !is.na(estimate)
     )
 
+
   return(list(mod_dat = dat_mod,
               mod_desc = mod_desc,
+              mod_desc_plot = mod_desc_plot,
               mod_diag = mod_diag,
               mod = mod,
               mod_output = mod_output))
