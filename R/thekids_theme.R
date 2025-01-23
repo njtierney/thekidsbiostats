e25254d3672befc626565c678314a637f9754861
wesdb <wesley.billingham@telethonkids.org.au>
2025-01-23 13:21:34 +0800

    Custom callout colours, fixed backslashes

--- R/thekids_theme.R
+++ R/thekids_theme.R
@@ -45,7 +45,7 @@
 #' 1. **Windows**:
 #'    - Download the fonts from [Google Fonts](https://fonts.google.com/specimen/Barlow+Semi+Condensed).
 #'    On Windows, the location is C:\\Windows\\Fonts.
-#'    (If you can’t move fonts there, use C:\\Users\\Username\\AppData\\Local\\Microsoft\\Windows\\Fonts\\
+#'    (If you can’t move fonts there, use C:\\\\Users\Username\AppData\Local\Microsoft\Windows\Fonts
 #'    to install fonts that can only be accessed by your own username.)
 #'    - Install the following font files:
 #'      - `BarlowSemiCondensed-ExtraBold.ttf`
@@ -170,7 +170,7 @@ thekids_theme <- function(base_size = 11, base_family = "Barlow Semi Condensed",
 #' 1. **Windows**:
 #'    - Download the fonts from [Google Fonts](https://fonts.google.com/specimen/Barlow+Semi+Condensed).
 #'    On Windows, the location is C:\\Windows\\Fonts.
-#'    (If you can’t move fonts there, use C:\\Users\\Username\\AppData\\Local\\Microsoft\\Windows\\Fonts\\
+#'    (If you can’t move fonts there, use C:\\\\Users\Username\AppData\Local\Microsoft\Windows\Fonts
 #'    to install fonts that can only be accessed by your own username.)
 #'    - Install the following font files:
 #'      - `BarlowSemiCondensed-ExtraBold.ttf`
