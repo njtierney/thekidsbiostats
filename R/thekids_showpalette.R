8caa004c79484f574cf273f88b6a5e73d69b59ba
Robin Cook <RobinCook9012@gmail.com>
2025-03-21 15:09:15 +0800

    Fixed typo in function name
    Changed function name to thekids_showpalette

--- R/thekids_showpalette.R
+++ R/thekids_showpalette.R
@@ -3,16 +3,16 @@
 #' @description Displays the colour palette for The Kids, including their hex codes.
 #'
 #' @export
-thekids_cols <- function() {
-  
+thekids_showpalette <- function() {
+
   bind_rows(thekids_palettes, .id = "Category") %>%
-    filter(Category != "typography") %>% 
+    filter(Category != "typography") %>%
     pivot_longer(-Category, names_to = "Color", values_to = "Hex") %>%
     drop_na() %>%  # Remove NA rows
     mutate(
       Category = factor(Category, levels = c("primary", "tint50", "tint10")),
       Color = factor(Color, levels = rev(names(thekids_palettes$primary))) # Maintain row order
-    ) %>% 
+    ) %>%
     ggplot(aes(x = Category, y = Color, fill = Hex)) +
     geom_tile(width = 0.45, height = 0.9) +  # Reduce width more than height to increase horizontal spacing
     labs(x = NULL, y = NULL, title = "The Kids Palette") +
@@ -27,6 +27,6 @@ thekids_cols <- function() {
       legend.position = "none"
     ) +
     coord_fixed(ratio = 0.5)  # Increase ratio to add more horizontal space
-  
+
 }
 
